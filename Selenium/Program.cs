664cc03cb181181cfbea3c209a0460bfe95a69d2
Dilan Ormisiar <dilan.ormisiar@gilmond.com>
2020-01-17 12:56:14 +0000

    Add project files.

---
+++ Selenium/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace Selenium
+{
+    class Program
+    {
+        private IWebDriver _webDriver = new ChromeDriver();
+        private IWebElement _webElement;
+
+        public void Main(string[] args)
+        {
+            var chromeDriver = new ChromeDriver();
+            var chromeWebElement = new ChromeWebElement(chromeDriver, "test");
+
+            
+
+            INavigation navigation = _webDriver.Navigate();
+            navigation.Back();
+
+            Environment
+            
+
+            var exception = new WebDriverException("there is something wrong");
+            string js = string.Format("window.scroll(0, {0});", link.Location.Y);
+            _webDriver.Quit();
+        }
+    }
+}
