57383449e588c500d08a03095322a62acd941aff
Dilan Ormisiar <dilan.ormisiar@gilmond.com>
2020-01-17 14:02:32 +0000

    events and other

--- Selenium/Program.cs
+++ Selenium/Program.cs
@@ -10,22 +10,26 @@ namespace Selenium
         private IWebDriver _webDriver = new ChromeDriver();
         private IWebElement _webElement;
 
+        public async event WhenScreenChanges(object run, EventArgs e);
+
         public void Main(string[] args)
         {
             var chromeDriver = new ChromeDriver();
             var chromeWebElement = new ChromeWebElement(chromeDriver, "test");
 
-            
+
 
             INavigation navigation = _webDriver.Navigate();
             navigation.Back();
 
-            Environment
-            
 
-            var exception = new WebDriverException("there is something wrong");
-            string js = string.Format("window.scroll(0, {0});", link.Location.Y);
-            _webDriver.Quit();
+
+        }
+
+        public async string WhenScreenChanges(object run, EventArgs)
+        {
+
         }
     }
 }
+
