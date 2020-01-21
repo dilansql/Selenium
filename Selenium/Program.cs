dffd9140f3175d1d425c71dc4874caa57ed0abe5
dilansql <dilan.ormisiar@gilmond.com>
2020-01-21 22:03:57 +0000

    EqualityImplementation

--- Selenium/Program.cs
+++ Selenium/Program.cs
@@ -8,15 +8,21 @@ namespace Selenium
     class Program
     {
         private static IWebDriver _webDriver = new ChromeDriver();
-        private static IWebElement _webElement;
         private static ChromeDriver ChromeDriver { get; set; } = new ChromeDriver();
-        private static WebDriverWait Wait { get; set; } = new WebDriverWait(ChromeDriver, new TimeSpan(50));
-        private ChromeWebElement ChromeWebElement {get; set;} = new ChromeWebElement(ChromeDriver, "test");
-        public void Main(string[] args)
+        private static IWebElement _webElement;
+        private static WebDriverWait Wait { get; set; } = new WebDriverWait(_webDriver, new TimeSpan(50));
+        public static void Main(string[] args)
         {
             INavigation navigation = _webDriver.Navigate();
             navigation.GoToUrl("https://digital-christmas-card.ballatoilet.repl.co/");
-            var submitButton = ChromeDriver.FindElementById("SubmitButton");
+            var submitButton = new ChromeWebElement(ChromeDriver, "SubmitButton");
+            var resetButton = new ChromeWebElement(ChromeDriver, "ResetButton");
+            var input = new ChromeWebElement(ChromeDriver, "NameInput");
+            var nameBar = new ChromeWebElement(ChromeDriver, "NameBar");
+            input.SendKeys("Test");
+            submitButton.Click();
+            var test = "Test";
+            test.Equals(nameBar.Text);
         }
     }
 }
