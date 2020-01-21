65c55596010abb5162a755a3e7b209b97ef93dfa
Dilan Ormisiar <dilan.ormisiar@gilmond.com>
2020-01-21 12:59:07 +0000

    WebDriverWait

--- Selenium/Program.cs
+++ Selenium/Program.cs
@@ -7,28 +7,29 @@ namespace Selenium
 {
     class Program
     {
-        private IWebDriver _webDriver = new ChromeDriver();
+        private static IWebDriver _webDriver = new ChromeDriver();
         private IWebElement _webElement;
 
-        public async event WhenScreenChanges(object run, EventArgs e);
-
-        public void Main(string[] args)
+        public static void Main(string[] args)
         {
             var chromeDriver = new ChromeDriver();
             var chromeWebElement = new ChromeWebElement(chromeDriver, "test");
+            WebDriverWait wait = new WebDriverWait(_webDriver, new TimeSpan(500));
 
-
+            var submitButton = wait.Until(ExpectedConditions.ElementToBeClickable(By.Name("SubmitButton")));
+            var input = wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("NameInput")));
+            var nameBar = wait.Until(ExpectedConditions.ElementToBeClickable(By.Id("NameBar")));
 
             INavigation navigation = _webDriver.Navigate();
-            navigation.Back();
-
-
-
-        }
-
-        public async string WhenScreenChanges(object run, EventArgs)
-        {
-
+            navigation.GoToUrl("https://digital-christmas-card.ballatoilet.repl.co/");
+            
+            input.SendKeys("Test");
+            submitButton.Click();
+            string test = "Test";
+            if (nameBar.Displayed.Equals(test))
+                Console.WriteLine("success");
+            else
+                Console.WriteLine($"Fail, the nameBar contains: {nameBar.Displayed}");
         }
     }
 }
